95511d8e79763f43554314cc0523bcf4bf98554e
Philipp Herrmann <herrmann.philipp00@gmail.com>
2022-12-15 17:06:29 +0100

    data_preprocessing.py removes rows with unknown values, saves new test and training set in "data_processed" as csv

---
+++ src/data_preprocessing.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import os
+from itertools import product
+
+"""Data Processing"""
+# Removed the first line "|1x3 Cross validator" in adult.test, because pd.read_csv failed and it was useless
+
+# 15 columns
+columns = ["age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation", "relationship",
+           "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "class"]
+
+if __name__ == "__main__":
+    for df_name in ["data", "test"]:
+        df = pd.read_csv("../data/adult." + df_name, names=columns, header=None)
+        # rows in df: train=32561, test=16281
+        indices_to_remove = [i for i, c in product(df.index, df.columns) if str(df.at[i, c]).__contains__("?")]
+        df = df.drop(indices_to_remove)
+        # rows in df: train=30162, test=15060
+        df.to_csv("../data_processed/" + df_name + ".csv")
