1ca5f2dd8fcf44f678e035d1941101c7d8c45d06
Anna Meyer <meyera@rhrk.uni-kl.de>
2022-12-28 11:34:00 +0100

    bar chart looks nice

--- src/training.py
+++ src/training.py
@@ -70,9 +70,33 @@ print(clf.score(test_input, test_labels))
 result = permutation_importance(clf, test_input, test_labels, n_repeats=10, random_state=0, n_jobs=1)
 forest_importances = pd.Series(result.importances_mean, index=train_input.columns)
 
+# use all onehotencoder features
 fig, ax = plt.subplots()
 forest_importances.plot.bar(yerr=result.importances_std, ax=ax)
 ax.set_title("Feature importances using permutation on full model")
 ax.set_ylabel("Mean accuracy decrease")
 fig.tight_layout()
 plt.show()
+
+# back to original features by summing their means
+features = ["age", "workclass", "education", "marital-status", "occupation", "relationship",
+           "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country"]
+
+for feature in features:
+    col_list = [i for i in train_input.columns if i.__contains__("_"+feature)]
+    forest_importances[feature] = forest_importances[col_list].sum(axis=0)
+    forest_importances = forest_importances.drop(col_list)
+    
+fig, ax = plt.subplots()
+forest_importances.plot.bar(ax=ax)
+ax.set_title("Feature importances using permutation on full model")
+ax.set_ylabel("Mean accuracy decrease")
+fig.tight_layout()
+plt.show()
+
+
+
+
+
+
+
